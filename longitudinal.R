be6dd99a5808acd6ecb3b651f93c5047feb39522
weston <westonslaughter@gmail.com>
2024-04-06 12:55:22 -0400

    plots and pre-watershed

--- longitudinal.R
+++ longitudinal.R
@@ -24,73 +24,81 @@ library(elevatr)
 library(rgeos)
 
 poi_usgs_id <- "USGS-01589300"
-poi <- c(-76.7331944, 39.34588889)
+poi_usgs_id_trim <- "01589300"
+
+site_info <- dataRetrieval::readNWISsite(poi_usgs_id_trim)
+
+site_name <- site_info$station_nm
+
+
+## poi <- c(-76.7331944, 39.34588889)
+poi <- c(site_info$dec_long_va, site_info$dec_lat_va)
 
 # create a point from decimal long/lat
-yose <- st_sfc(st_point(poi), crs = 4326)
+hgsite <- st_sfc(st_point(poi), crs = 4326)
 
 # check class is "sfc" and "sfc_POINT"
-class(yose)
+class(hgsite)
 
 # now figure out the nearest stream segment ID to our point
-(yose_comid <- discover_nhdplus_id(yose))
+(hgsite_comid <- discover_nhdplus_id(hgsite))
 
 # first make a list defining the sourcetype and ID
-yose_list <- list(featureSource = "comid", 
-                  featureID = yose_comid)
+hgsite_list <- list(featureSource = "comid",
+                  featureID = hgsite_comid)
 
 # get upstream flowlines
-yose_us_flowlines <- navigate_nldi(nldi_feature = yose_list,
+hgsite_us_flowlines <- navigate_nldi(nldi_feature = hgsite_list,
                                    mode = "UT",
                                    distance = 100,
                                    data_source = "")
 
 # get downstream mainstem only (from our starting segment):
-yose_ds_flowlines <- navigate_nldi(nldi_feature = yose_list, 
+hgsite_ds_flowlines <- navigate_nldi(nldi_feature = hgsite_list,
                                    mode = "DM", 
                                    distance_km = 0,
                                    data_source = "")
 
 
 # make a list of all the comids we've identified:
-# all_comids <- c(yose_us_flowlines$UT_flowlines$nhdplus_comid, yose_ds_flowlines$DM_flowlines$nhdplus_comid)
-all_comids <- c(yose_us_flowlines$UT_flowlines$nhdplus_comid)
+# all_comids <- c(hgsite_us_flowlines$UT_flowlines$nhdplus_comid, hgsite_ds_flowlines$DM_flowlines$nhdplus_comid)
+all_comids <- c(hgsite_us_flowlines$UT_flowlines$nhdplus_comid)
 
 # download all data and create a geopackage with the comid list
-yose_gpkg <- subset_nhdplus(comids= as.integer(all_comids),
+hgsite_gpkg <- subset_nhdplus(comids= as.integer(all_comids),
                             simplified = TRUE,
                             overwrite = TRUE,
-                            output_file = paste0(here::here(), "/data/yose_nhdplus.gpkg"),
+                            output_file = paste0(here::here(), "/data/hgsite_nhdplus.gpkg"),
                             nhdplus_data = "download",
                             return_data = FALSE)
 
 # check layers in database:
-st_layers(paste0(here::here(), "/data/yose_nhdplus.gpkg"))
+st_layers(paste0(here::here(), "/data/hgsite_nhdplus.gpkg"))
 
 # pull the flowlines back in
-yose_streams <- read_sf(paste0(here::here(), "/data/yose_nhdplus.gpkg"), "NHDFlowline_Network")
+hgsite_streams <- read_sf(paste0(here::here(), "/data/hgsite_nhdplus.gpkg"), "NHDFlowline_Network")
 
 # make a map
 prettymapr::prettymap({
   rosm::osm.plot(project = FALSE, 
-                 bbox = matrix(st_bbox(yose_streams), byrow = FALSE, ncol = 2, 
+                 bbox = matrix(st_bbox(hgsite_streams), byrow = FALSE, ncol = 2,
                                dimnames = list(c("x", "y"), c("min", "max"))), 
                  type = "cartolight", quiet = TRUE, progress = "none")
-  plot(yose_streams$geom, col = "steelblue", lwd = (yose_streams$streamorde / 4), add=TRUE)
-  plot(yose, add=TRUE, pch=21, bg="orange", cex=1.5)
+  plot(hgsite_streams$geom, col = "steelblue", lwd = (hgsite_streams$streamorde / 4), add=TRUE)
+  plot(hgsite, add=TRUE, pch=21, bg="orange", cex=1.5)
   prettymapr::addnortharrow()
 })
 
 # find upstream gages
-yose_us_gages <- navigate_nldi(yose_list,
+hgsite_us_gages <- navigate_nldi(hgsite_list,
                                mode = "UT",
                                data_source = "nwissite")
 
 # get downstream everything from our only upstream gage (Happy Isles)
 usgs_point <- list(featureSource="nwissite", featureID = "USGS-11264500")
 
-# find all downstream gages on the mainstem river (Merced/San Joaquin)
-yose_ds_gages <- navigate_nldi(yose_list,
+# find all downstream gages on the mainstem river (Hgriver/San Joaquin)
+hgsite_ds_gages <- navigate_nldi(hgsite_list,
                                mode = "DM",
                                #distance_km = 50,
                                data_source = "nwissite",
@@ -98,39 +106,39 @@ yose_ds_gages <- navigate_nldi(yose_list,
 
 # let's add these data to our geopackage as well
 # remember it's best to have everything in the same projection
-if(st_crs(yose_streams) != st_crs(yose_us_gages)) {
-    sf::st_set_crs(yose_us_gages$UT_nwissite, st_crs(yose_streams))
-    sf::st_set_crs(yose_ds_gages$DM_nwissite, st_crs(yose_streams))
+if(st_crs(hgsite_streams) != st_crs(hgsite_us_gages)) {
+    sf::st_set_crs(hgsite_us_gages$UT_nwissite, st_crs(hgsite_streams))
+    sf::st_set_crs(hgsite_ds_gages$DM_nwissite, st_crs(hgsite_streams))
   }
 
 # write to geopackage: overwite the layer if it exists
-st_write(yose_us_gages$UT_nwissite, dsn=paste0(here::here(),"/data/yose_nhdplus.gpkg"), 
-         layer="yose_us_gages", append = FALSE, delete_layer = TRUE)
+st_write(hgsite_us_gages$UT_nwissite, dsn=paste0(here::here(),"/data/hgsite_nhdplus.gpkg"),
+         layer="hgsite_us_gages", append = FALSE, delete_layer = TRUE)
 
-st_write(yose_ds_gages$DM_nwissite, dsn=paste0(here::here(),"/data/yose_nhdplus.gpkg"), 
-         layer="yose_ds_gages", append = FALSE, delete_layer = TRUE)
+st_write(hgsite_ds_gages$DM_nwissite, dsn=paste0(here::here(),"/data/hgsite_nhdplus.gpkg"),
+         layer="hgsite_ds_gages", append = FALSE, delete_layer = TRUE)
 
 # check layers:
-st_layers(paste0(here::here(), "/data/yose_nhdplus.gpkg"))
+st_layers(paste0(here::here(), "/data/hgsite_nhdplus.gpkg"))
 
-m1 <- mapview(yose, col.regions="black", cex=6, layer.name="Start Point") + 
-  mapview(yose_streams, zcol="streamorde", legend=TRUE, layer.name="Stream <br> Order") + 
-  mapview(yose_us_gages, col.regions="orange", layer.name="U/S Gage") +
-  mapview(yose_ds_gages, col.regions="maroon", layer.name="D/S Gages")
+m1 <- mapview(hgsite, col.regions="black", cex=6, layer.name="Start Point") +
+  mapview(hgsite_streams, zcol="streamorde", legend=TRUE, layer.name="Stream <br> Order") +
+  mapview(hgsite_us_gages, col.regions="orange", layer.name="U/S Gage") +
+  mapview(hgsite_ds_gages, col.regions="maroon", layer.name="D/S Gages")
 
 # add a measurement tool
 m1@map %>% leaflet::addMeasure(primaryLengthUnit = "kilometers") %>%
   leaflet.extras::addFullscreenControl(position = "topleft")
 
 # Project first (to ensure using nngeo::nn2, otherwise lat/lon is similar to st_distance)
-merced_us_gage <- st_transform(yose_us_gages$UT_nwissite, 26910)
+hgriver_us_gage <- st_transform(hgsite_us_gages$UT_nwissite, 26910)
 
 # get the most downstream gage (find ID using mapview map)
-merced_ds_gage <- yose_ds_gages$DM_nwissite %>% 
+hgriver_ds_gage <- hgsite_ds_gages$DM_nwissite %>%
   filter(identifier=="USGS-01589350") %>% st_transform(26910)
 
 # calculate the max euclidean (straight line) distance in meters
-max_gage_dist <- st_nn(merced_ds_gage, merced_us_gage, 
+max_gage_dist <- st_nn(hgriver_ds_gage, hgriver_us_gage,
                        returnDist = TRUE, progress = FALSE) 
 
 # now convert this measurement to km
@@ -167,28 +175,28 @@ st_snap_points <- function(x, y, namevar, max_dist = 1000) {
 }
 
 # first lets merge all our gages into one dataframe. Make sure in same crs
-st_crs(yose_us_gages$UT_nwissite) == st_crs(yose_ds_gages$DM_nwissite)
+st_crs(hgsite_us_gages$UT_nwissite) == st_crs(hgsite_ds_gages$DM_nwissite)
 
 # now bind together
-all_gages <- rbind(yose_us_gages$UT_nwissite, yose_ds_gages$DM_nwissite) 
+all_gages <- rbind(hgsite_us_gages$UT_nwissite, hgsite_ds_gages$DM_nwissite)
 
 # check for duplicates
 all_gages %>% distinct(identifier) %>% nrow()
 
 # # try using different numbers of cols in each dataframe (but keep geom in both)
 # all_gages <- st_as_sf(data.table::rbindlist(
-#   list(yose_us_gages[,c(2,6,8)], yose_ds_gages), fill = TRUE))
+#   list(hgsite_us_gages[,c(2,6,8)], hgsite_ds_gages), fill = TRUE))
 
-# note there are NAs in the columns that were missing from the yose_us_gage dataframe.
+# note there are NAs in the columns that were missing from the hgsite_us_gage dataframe.
 
 # first project
 all_gages_proj <- st_transform(all_gages, crs = 26910)
-yose_streams_proj <- st_transform(yose_streams, crs=26910)
-yose_proj <- st_transform(yose, crs=26910)  
+hgsite_streams_proj <- st_transform(hgsite_streams, crs=26910)
+hgsite_proj <- st_transform(hgsite, crs=26910)
 
 # now snap points to the lines using a 500 meter buffer, select which ID column you want keep for rejoining
 gages_snapped <- st_snap_points(all_gages_proj, 
-                                yose_streams_proj, 
+                                hgsite_streams_proj,
                                 namevar = "identifier", 
                                 max_dist = 500)
 
@@ -196,7 +204,7 @@ gages_snapped <- st_snap_points(all_gages_proj,
 poi_snapped <- gages_snapped[gages_snapped$identifier == poi_usgs_id,]
 
 mapview(gages_snapped, col.regions="cyan", layer.name="Snapped Gages") +
-  mapview(yose_streams_proj, color="steelblue", layer.name="Flowlines") + 
+  mapview(hgsite_streams_proj, color="steelblue", layer.name="Flowlines") +
   mapview(all_gages, col.regions="orange", layer.name="All Gages")
 
 
@@ -205,11 +213,11 @@ gages_snapped_buff <- st_buffer(gages_snapped, 1)
 poi_snapped_buff <-  st_buffer(poi_snapped, 5)
 
 # now use lwgeom::st_split to split stream segments
-# segs <- st_collection_extract(lwgeom::st_split(yose_streams_proj, gages_snapped_buff), "LINESTRING") %>% 
+# segs <- st_collection_extract(lwgeom::st_split(hgsite_streams_proj, gages_snapped_buff), "LINESTRING") %>%
 
-mapview(yose_streams_proj)
+mapview(hgsite_streams_proj)
 
-segs <- st_collection_extract(lwgeom::st_split(yose_streams_proj, poi_snapped_buff[1,]), "LINESTRING") %>%
+segs <- st_collection_extract(lwgeom::st_split(hgsite_streams_proj, poi_snapped_buff[1,]), "LINESTRING") %>%
   tibble::rownames_to_column(var = "rowid") %>% 
   mutate(rowid=as.integer(rowid))
 
@@ -273,12 +281,46 @@ segs_hr <- st_collection_extract(lwgeom::st_split(distinct(segs_filt), st_buffer
   )
 
 segs_hr_calc <- segs_hr %>%
-    group_by(as.character(round(total_len_km, 1))) %>%
+    group_by(as.character(round(total_len_km, 0))) %>%
     summarize(
       elevation = mean(elevation),
       longitudinal_km = mean(total_len_km)
+    ) %>%
+    mutate(
+      elevation_change = c(diff(elevation), NA),
+      longitudinal_km_change = c(diff(longitudinal_km), NA),
+      gradient = elevation/longitudinal_km_change,
+      gradient_log10 = log10(gradient)
     )
 
-# plot
+# plot D by E
 ggplot(segs_hr_calc) +
-  geom_point(aes(x = scale(longitudinal_km), y = elevation))
+  geom_point(aes(x = longitudinal_km, y = elevation, col = longitudinal_km), size = 2.75) +
+  ggtitle(paste('Elevation (m) Along Flowpath (km) at', site_name),
+          subtitle = paste("USGS", site_info$site_no)) +
+  scale_color_viridis() +
+  theme_minimal() +
+  theme(
+    text = element_text(size = 26)
+  )
+
+# plot gradient
+ggplot(segs_hr_calc) +
+  geom_point(aes(x = longitudinal_km, y = gradient_log10, col = elevation), size = 2.75) +
+  ggtitle(paste('Gradient (log10) Along Flowpath (km) at', site_name),
+          subtitle = paste("USGS", site_info$site_no)) +
+  scale_color_viridis() +
+  theme_minimal() +
+  theme(
+    text = element_text(size = 26)
+  )
+
+
+## Delineate watersed
+out <- ms_delineate_watershed(
+    lat = site_info$dec_lat_va,
+    long = site_info$dec_long_va,
+    crs = 4269,
+    write_dir = '/your/path',
+    write_name = 'example_site'
+)
